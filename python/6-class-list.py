5167651cfbbabc8548cc89947935deef2623d28d
kishanYuvmedia <142478981+kishanYuvmedia@users.noreply.github.com>
2026-05-08 15:09:49 +0530

    data- list syntex

---
+++ python/6-class-list.py
@@ -0,0 +1,38 @@
+city=['New York', 'London', 'Tokyo', 'Paris', 'Sydney']
+
+# for i in city:
+#     print(i)
+
+# for v in city[2:4]:
+#     print(v)
+# print(len(city))
+# number=3
+# print(range(0, 7))
+
+# for i in range(0, len(city)):
+#     print(i, city[i])
+
+# a="rajesh"
+# print(a)
+
+# while len(city) > 0:
+#     print(city)
+
+# for i in range(10,-1,-1):
+#     print(i)
+
+# dict1={'name':'rajesh','age':30,'city':'New York'}
+# print(dict1['age'])
+# for key in dict1:
+#     print(key, dict1[key])
+
+
+a=10
+name="rajesh"
+print("The value of a is:", a)
+print(f"your name is :-{name} age is :-{a}")
+print("Hello" \
+"World")
+print('''hello 
+      ajmer
+      word''')
